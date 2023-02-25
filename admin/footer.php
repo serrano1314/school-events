3a8d18ce7ff690178a0537ec2cc87dba506b22da
Steven Serrano <serrano1314@gmail.com>
2023-02-25 14:17:55 +0800

    move script tags to header

--- admin/footer.php
+++ admin/footer.php
@@ -6,14 +6,6 @@
 
 </body>
 
-<script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.4/index.global.min.js'></script>
-<script src="https://unpkg.com/gridjs/dist/gridjs.umd.js"></script>
-<script src="admin.js"></script>
-<!-- bootstrap  -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-<!-- jQuery  -->
-<script src="https://code.jquery.com/jquery-3.6.3.min.js"crossorigin="anonymous"></script>
 
-<script src="admin.js"></script>
 
 </html>
--- admin/header.php
+++ admin/header.php
@@ -10,16 +10,18 @@
     
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/admintest.css">
-    <!-- <link href="https://unpkg.com/gridjs/dist/theme/mermaid.min.css" rel="stylesheet"> -->
+    
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/jqc-1.12.4/dt-1.13.1/datatables.min.css"/>
 
-    <!-- bootstrap  -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.3.min.jss"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/jqc-1.12.4/dt-1.13.1/datatables.min.js"></script>
 
     <script src="https://kit.fontawesome.com/4667e2671f.js" crossorigin="anonymous"></script>
-    <!-- <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> -->
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     
+
+    <script src="admin.js"></script>
 
     
 </head>
